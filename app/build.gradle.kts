c2e21485f6bd63e07f05e4b868fe2cbb305318e7
Colin Frick <colin@frick.io>
2024-01-17 10:14:31 +0100

    fix: set manifestPlaceholders

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,17 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "io.fusionauth.sdk-test"
+        applicationId = "io.fusionauth.sdktest"
         minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Make sure this is consistent with the redirect URI used in res/raw/auth_config.json,
+        // or specify additional redirect URIs in AndroidManifest.xml
+        manifestPlaceholders["appAuthRedirectScheme"] = "io.fusionauth.sdktest-demo"
     }
 
     buildTypes {
@@ -33,6 +37,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,6 +48,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation(project(":MobileSDK"))
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
