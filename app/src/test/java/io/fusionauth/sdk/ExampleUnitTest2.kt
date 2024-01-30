3ef154605c0a0726bb5e8e676f043edb09687aa3
Aaron-Ritter <git@ritter.world>
2024-01-30 14:27:32 +0100

    ci: test vulnerability reviewdog

---
+++ app/src/test/java/io/fusionauth/sdk/ExampleUnitTest2.kt
@@ -0,0 +1,17 @@
+package io.fusionauth.sdk
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest2 {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
