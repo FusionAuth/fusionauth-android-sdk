364521047582f945ac3dd44884c47a468a25e51e
Colin Frick <colin@frick.io>
2024-02-08 15:01:10 +0100

    refactor: detekt

--- MobileSDK/build.gradle.kts
+++ MobileSDK/build.gradle.kts
@@ -13,6 +13,8 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+
+        manifestPlaceholders["appAuthRedirectScheme"] = "io.fusionauth.app"
     }
 
     buildTypes {
--- MobileSDK/src/test/java/io/fusionauth/mobilesdk/ExampleUnitTest.kt
+++ MobileSDK/src/test/java/io/fusionauth/mobilesdk/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package io.fusionauth.mobilesdk
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
