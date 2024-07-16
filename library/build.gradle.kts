961b4a7becc408e4331581c29398ad7e0d967d60
Aaron-Ritter <git@ritter.world>
2024-07-16 15:43:10 +0200

    chore(release): 0.1.7 🎉

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -71,7 +71,7 @@ publishing {
                 // referenced as ${groupId}:${artifactId}.
                 groupId = "io.fusionauth"
                 artifactId = "fusionauth-android-sdk"
-                version = "0.1.7-4"
+                version = "0.1.7"
 
                 // And here are some more properties that go into the pom file.
                 // For a full list of required metadata fields, see:
