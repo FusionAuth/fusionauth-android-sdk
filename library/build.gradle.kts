772367466fba098af7f1a197c1cfcc04d1f1ed15
Mike Rudat <mike.rudat@fusionauth.io>
2025-09-02 13:35:27 -0600

    Update library to the latest version.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -173,7 +173,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.18.0")
+    testImplementation("org.mockito:mockito-core:5.19.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:6.0.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
