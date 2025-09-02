16e76f9ed8426af733c358a12119e6f4493d14c0
Mike Rudat <mike.rudat@fusionauth.io>
2025-09-02 14:29:21 -0600

    ensureInitialized() method accesses the isInitialized field without synchronization, which could lead to race conditions.

--- library/src/main/java/io/fusionauth/mobilesdk/AuthorizationManager.kt
+++ library/src/main/java/io/fusionauth/mobilesdk/AuthorizationManager.kt
@@ -205,7 +205,7 @@ object AuthorizationManager {
         ensureInitialized()
         return configuration
     }
-    private fun ensureInitialized() {
+    @Synchronized private fun ensureInitialized() {
         if (!isInitialized) {
             throw AuthorizationException("AuthorizationManager must be initialized by calling initialize() first.")
         }
