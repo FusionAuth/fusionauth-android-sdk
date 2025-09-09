394cc1e1dc3678d0a3a2171e05f54b4121ade9fb
Mike Rudat <mike.rudat@fusionauth.io>
2025-09-09 10:14:16 -0600

    mobsfscan doesn't like the exception being logged.  datekt doesn't like the exception being swallowed.

--- app/src/main/java/io/fusionauth/sdk/TokenActivity.kt
+++ app/src/main/java/io/fusionauth/sdk/TokenActivity.kt
@@ -296,7 +296,7 @@ class TokenActivity : AppCompatActivity() {
                     AuthorizationConfiguration.fromResources(
                         this@TokenActivity, resource))
             } catch (ex: AuthorizationException) {
-                Log.e(TAG, "Failed to set the auth configuration")
+                Log.e(TAG, "Failed to set the auth configuration", ex)
                 showSnackbar("Failed to set the auth configuration -  " + ex.message)
             }
         }
