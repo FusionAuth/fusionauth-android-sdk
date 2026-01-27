378bb619833f7c3f014414aed3184d06ba931618
Mike Rudat <mike.rudat@fusionauth.io>
2026-01-27 13:18:41 -0700

    chore(release): v1.0.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -76,7 +76,7 @@ publishing {
                 groupId = "io.fusionauth"
                 artifactId = "fusionauth-android-sdk"
                 // x-release-please-start-version
-                version = "1.0.0-rc"
+                version = "1.0.0"
                 // x-release-please-end
 
                 // And here are some more properties that go into the pom file.
